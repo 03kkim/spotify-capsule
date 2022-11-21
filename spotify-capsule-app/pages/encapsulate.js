2c480093e1329c53cbd6934c3e2bba5b238fe3c8
03kkim <theq614@gmail.com>
2022-11-20 23:05:30 -0600

    add encapsulate and Link to get there

---
+++ spotify-capsule-app/pages/encapsulate.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Encapsulate() {
+    return (
+        <div>
+        <Head>
+            <title>Encapsulate!</title>
+            <meta name="description" content="Make your capsule" />
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
+        
+        </div>
+    )
+}
