508270ca6d2aca7cfa598a5fb92aea39d614b2c8
03kkim <theq614@gmail.com>
2022-11-21 21:49:33 -0600

    add theme and emotion

---
+++ spotify-capsule-app/styles/theme/darkTheme.js
@@ -0,0 +1,12 @@
+import { createTheme } from '@mui/material/styles';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+  spotify: {
+    green: '#1DB954',
+  }
+});
+
+export default darkTheme;
