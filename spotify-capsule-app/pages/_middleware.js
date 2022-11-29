b8091413bf5d1a782d4146b6813d695521f8e432
Micah Casey-Fusco <mifyedev@gmail.com>
2022-11-28 20:01:25 -0600

    add middleware

---
+++ spotify-capsule-app/pages/_middleware.js
@@ -0,0 +1,26 @@
+import { getToken }  from "next-auth/jwt";
+import { NextResponse } from "next/server";
+
+
+export async function middleware(req) {
+    // Token will exist if user is logged in
+    const token = await getToken({ req, secret: process.env.JWT_SECRET});
+
+    const { pathname } = req.nextUrl
+
+    // Allow request if the following is true..
+    // 1) Its a request for the next-auth session & provider fetching
+    // 2) The token exists
+    if (pathname.includes("/api/auth") || token) {
+        return NextResponse.next();
+    }
+
+    // Redirect to landing page to login if they don't have a token && are requesting a protected route
+    if (!token && pathname != "/index") {
+        return NextResponse.redirect("/");
+    }
+
+
+
+
+}
