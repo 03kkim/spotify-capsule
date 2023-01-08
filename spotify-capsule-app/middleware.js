d7b5ff2b3e4be18e6b4b6ebb7a3f0d90ec3d915e
03kkim <theq614@gmail.com>
2023-01-08 11:23:41 -0500

    add matcher line to middleware.js

--- spotify-capsule-app/middleware.js
+++ spotify-capsule-app/middleware.js
@@ -1,22 +1,25 @@
-import { getToken }  from "next-auth/jwt";
+import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
-import { NextRequest } from 'next/server';
-
+import { NextRequest } from "next/server";
 
 export async function middleware(req) {
-    // Token will exist if user is logged in
-    const token = await getToken({ req, secret: process.env.JWT_SECRET});
+  // Token will exist if user is logged in
+  const token = await getToken({ req, secret: process.env.JWT_SECRET });
 
-    const { pathname, origin } = req.nextUrl
+  const { pathname, origin } = req.nextUrl;
 
-    // Allow request if the following is true..
-    // 1) Its a request for the next-auth session & provider fetching
-    // 2) The token exists
-    if (pathname.includes("/api/auth") || token) {
-        return NextResponse.next();
-    }
-    
-    if (!token && pathname == '/encapsulate') {
-        return NextResponse.redirect(`${origin}`);
-    }
+  // Allow request if the following is true..
+  // 1) Its a request for the next-auth session & provider fetching
+  // 2) The token exists
+  if (pathname.includes("/api/auth") || token) {
+    return NextResponse.next();
+  }
+
+  if (!token && pathname == "/encapsulate") {
+    return NextResponse.redirect(`${origin}`);
+  }
 }
+
+export const config = {
+  matcher: "/",
+};
